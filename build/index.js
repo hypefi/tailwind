5bc41acd11c64979b33da86afdef9f06e6a5e1fb
hypefi <anasboukharta@gmail.com>
2020-11-21 11:46:39 +0100

    jquery add and play with header

--- build/index.js
+++ build/index.js
@@ -1,3 +1,6 @@
+//import $ from 'jquery';
+//import $ from './libs/jquery/dist/jquery.js'
+/*
 function navToggle() {
     var btn = document.getElementById('button1');
     var nav = document.getElementById('flyout1');
@@ -5,4 +8,23 @@ function navToggle() {
     btn.classList.toggle('open');
     nav.classList.toggle('flex');
     nav.classList.toggle('hidden');
-}
+}*/
+
+$("#flyout1").css('visibility','hidden')
+$("#flyout2").css('visibility','hidden')
+
+
+$("#button1").hover(fadeIn, fadeOut);
+
+function fadeIn() {
+      $("#flyout1").css('visibility','visible')
+      $("#flyout1").fadeIn( 100 );
+  }
+
+function fadeOut() {
+      $("#flyout1").fadeOut( 100 );
+  }
+
+
+$("#button2").hover(()=> {      $("#flyout2").css('visibility','visible'); $("#flyout2").fadeIn( 100 )}, ()=> $("#flyout2").fadeOut( 100 ));
+
