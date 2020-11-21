e83c3c369e642be1058c47351c43896ea3dfe3d5
hypefi <anasboukharta@gmail.com>
2020-11-21 12:15:59 +0100

    jquery fade in menu working can make it better time out check

--- build/index.js
+++ build/index.js
@@ -14,17 +14,20 @@ $("#flyout1").css('visibility','hidden')
 $("#flyout2").css('visibility','hidden')
 
 
-$("#button1").hover(fadeIn, fadeOut);
-
-function fadeIn() {
-      $("#flyout1").css('visibility','visible')
-      $("#flyout1").fadeIn( 100 );
-  }
-
-function fadeOut() {
-      $("#flyout1").fadeOut( 100 );
-  }
 
 
-$("#button2").hover(()=> {      $("#flyout2").css('visibility','visible'); $("#flyout2").fadeIn( 100 )}, ()=> $("#flyout2").fadeOut( 100 ));
 
+$("#button1").hover(()=> {      $("#flyout1").css('visibility','visible'); $("#flyout1").fadeIn( 100 )});
+//$("#flyout2").hover(()=> {   $("#flyout2").show()}, ()=> $("#flyout2").fadeOut( 500 ));
+$( "#flyout1" ).mouseleave(()=> $("#flyout1").fadeOut( 500 ))
+$( "#button1" ).mouseleave(() => setTimeout(function() {
+        $("#flyout1").hide()
+    }, 5000));
+
+
+$("#button2").hover(()=> {      $("#flyout2").css('visibility','visible'); $("#flyout2").fadeIn( 100 )});
+//$("#flyout2").hover(()=> {   $("#flyout2").show()}, ()=> $("#flyout2").fadeOut( 500 ));
+$( "#flyout2" ).mouseleave(()=> $("#flyout2").fadeOut( 500 ))
+$( "#button2" ).mouseleave(() => setTimeout(function() {
+        $("#flyout2").hide()
+    }, 5000));
